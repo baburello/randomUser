bb278a0615439f80bf7f003baa3142f6cde02fec
baburello <babur565@gmail.com>
2024-01-23 17:57:35 +0500

    feat: loader add

--- js/request.js
+++ js/request.js
@@ -3,3 +3,39 @@ const API = 'https://randomuser.me/api/?results=9'
 
 // for leader
 const overlay = document.getElementById('overlay')
+
+//loader
+const loaderToggle = (toggle) => {
+    if (toggle) {
+        overlay.classList.remove('hidden')
+    } else {
+        overlay.classList.add('hidden')
+    }
+}
+
+//request promise
+
+const getData = (resourse) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
+
+
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState < 4) {
+                loaderToggle(true)
+            } else if (request.readyState == 4 && request.status == 200) {
+                const data = JSON.parse(request.responseText)
+                console.log(data)
+                loaderToggle(false)
+            } else if (request.readyState == 4) {
+                console.log("error")
+                loaderToggle(false)
+            }
+        })
+
+        request.open('GET', resourse)
+        request.send()
+    })
+}
+
+getData(API)
