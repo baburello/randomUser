6a6fb725e9e872472c8090526a1ac9e212c35c02
baburello <babur565@gmail.com>
2024-01-23 16:06:17 +0500

    feat: light and dark mode enable

--- js/mode.js
+++ js/mode.js
@@ -1,3 +1,16 @@
 const body = document.querySelector('body')
 const darkBtn = document.getElementById('dark-btn')
 const lightBtn = document.getElementById('light-btn')
+
+darkBtn.addEventListener('click', () => {
+    darkBtn.classList.add("hidden")
+    lightBtn.classList.remove('hidden')
+    body.classList.add('dark-mode')
+})
+
+lightBtn.addEventListener('click', () => {
+    lightBtn.classList.add("hidden")
+    darkBtn.classList.remove('hidden')
+    body.classList.remove('dark-mode')
+})
+
